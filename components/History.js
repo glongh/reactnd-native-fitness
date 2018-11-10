7c97954113d8e7f5bba7d24e9517fda622aa211f
glongh <glongh@gmail.com>
2018-11-10 15:59:33 -0500

    feat: Improving loading UX with an AppLoading component

--- components/History.js
+++ components/History.js
@@ -13,8 +13,13 @@ import { fetchCalendarResults } from '../utils/api';
 import UdaciFitnessCalendar from 'udacifitness-calendar';
 import { white } from '../utils/colors';
 import DateHeader from './DateHeader';
+import MetricCard from './MetricCard';
+import { AppLoading } from 'expo';
 
 class History extends Component {
+  state = {
+    ready: false
+  };
   componentDidMount() {
     const { dispatch } = this.props;
 
@@ -40,7 +45,7 @@ class History extends Component {
         </View>
       ) : (
         <TouchableOpacity onPress={() => console.log('Pressed!')}>
-          <Text>{JSON.stringify(metrics)}</Text>
+          <MetricCard date={formattedDate} metrics={metrics} />
         </TouchableOpacity>
       )}
     </View>
@@ -58,6 +63,11 @@ class History extends Component {
 
   render() {
     const { entries } = this.props;
+    const { ready } = this.state;
+    
+    if (ready === false) {
+        return <AppLoading />
+    }
 
     return (
       <UdaciFitnessCalendar
