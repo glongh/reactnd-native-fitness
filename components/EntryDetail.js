c0e26dce52f66bc5286ba7e6c9190a84a6e51d2b
glongh <glongh@gmail.com>
2018-11-17 14:30:48 -0500

    feat: Add reset functionality to EntryDetail component

--- components/EntryDetail.js
+++ components/EntryDetail.js
@@ -1,51 +1,83 @@
 import React, { Component } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import { connect } from 'react-redux';
-import { timeToString, getDailyReminderValue } from '../utils/helpers'
+import { timeToString, getDailyReminderValue } from '../utils/helpers';
 import MetricCard from './MetricCard';
-import { white } from '../utils/helpers'; 
+import { white } from '../utils/helpers';
+import { addEntry } from '../actions';
+import { removeEntry } from '../utils/api';
+import TextButton from './TextButton';
 
 class EntryDetail extends Component {
-    static navigationOptions = ({ navigation }) => {
-        const { entryId } = navigation.state.params
+  static navigationOptions = ({ navigation }) => {
+    const { entryId } = navigation.state.params;
 
-        const year = entryId.slice(0,4)
-        const month = entryId.slice(5,7)
-        const day = entryId.slice(8)
+    const year = entryId.slice(0, 4);
+    const month = entryId.slice(5, 7);
+    const day = entryId.slice(8);
 
-        return {
-            title: `${month}/${day}/${year}`
-        }
-    }
-    render() {
-        const { metrics } = this.props
-        return (
-        <View style={styles.container}>
-            <MetricCard metrics={metrics} />
-            <Text>
-            Entry Detail -{' '}
-            {JSON.stringify(this.props.navigation.state.params.entryId)}
-            </Text>
-        </View>
-        );
-    }
+    return {
+      title: `${month}/${day}/${year}`
+    };
+  };
+
+  reset = () => {
+      const { remove, goBack, entryId } = this.props
+
+      remove()
+      goBack()
+      removeEntry(entryId)
+  }
+
+  shouldComponentUpdate (nextProps) {
+      return nextProps.metrics !== null && !nextProps.metrics.today
+  }
+
+  render() {
+    const { metrics } = this.props;
+    return (
+      <View style={styles.container}>
+        <MetricCard metrics={metrics} />
+        <TextButton style={{ margin: 20 }} onPress={this.reset}>
+          RESET
+        </TextButton>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: white,
-        padding: 15
-    },
-})
+  container: {
+    flex: 1,
+    backgroundColor: white,
+    padding: 15
+  }
+});
 
-function mapStateToProps (state, { navigation }) {
-    const { entryId } = navigation.state.params
+function mapStateToProps(state, { navigation }) {
+  const { entryId } = navigation.state.params;
 
-    return {
-        entryId,
-        metrics: state[entryId]
-    }
+  return {
+    entryId,
+    metrics: state[entryId]
+  };
 }
 
-export default connect(mapStateToProps)(EntryDetail);
+function mapDispatchToProps(dispatch, { navigation }) {
+  const { entryId } = navigation.state.params;
+
+  return {
+    remove: () =>
+      dispatch(
+        addEntry({
+          [entryId]: timeToString() === entryId ? getDailyReminderValue() : null
+        })
+      ),
+    goBack: () => navigation.goBack()
+  };
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(EntryDetail);
