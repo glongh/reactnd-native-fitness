d0c18f7f7bff3da2f2e7e9b93d6145769e2e7c0a
glongh <glongh@gmail.com>
2018-11-18 15:49:51 -0500

    feat: Set Local notifications

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,12 +1,19 @@
-import React from 'react'
-import { View, StyleSheet } from 'react-native'
-import { FontAwesome, MaterialIcons, MaterialCommunityIcons } from '@expo/vector-icons'
-import { red, orange, blue, lightPurp, pink, white } from './colors'
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+import {
+  FontAwesome,
+  MaterialIcons,
+  MaterialCommunityIcons
+} from '@expo/vector-icons';
+import { red, orange, blue, lightPurp, pink, white } from './colors';
+import { Notifications, Permissions } from 'expo';
 
-export function getDailyReminderValue () {
+const NOTIFICATION_KEY = 'UdaciFitness:notifications';
+
+export function getDailyReminderValue() {
   return {
     today: "👋 Don't forget to log your data today!"
-  }
+  };
 }
 
 const styles = StyleSheet.create({
@@ -18,10 +25,10 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     marginRight: 20
-  },
-})
+  }
+});
 
-export function getMetricMetaInfo (metric) {
+export function getMetricMetaInfo(metric) {
   const info = {
     run: {
       displayName: 'Run',
@@ -31,14 +38,10 @@ export function getMetricMetaInfo (metric) {
       type: 'steppers',
       getIcon() {
         return (
-          <View style={[styles.iconContainer, {backgroundColor: red}]}>
-            <MaterialIcons
-              name='directions-run'
-              color={white}
-              size={35}
-            />
+          <View style={[styles.iconContainer, { backgroundColor: red }]}>
+            <MaterialIcons name="directions-run" color={white} size={35} />
           </View>
-        )
+        );
       }
     },
     bike: {
@@ -49,14 +52,10 @@ export function getMetricMetaInfo (metric) {
       type: 'steppers',
       getIcon() {
         return (
-          <View style={[styles.iconContainer, {backgroundColor: orange}]}>
-            <MaterialCommunityIcons
-              name='bike'
-              color={white}
-              size={32}
-            />
+          <View style={[styles.iconContainer, { backgroundColor: orange }]}>
+            <MaterialCommunityIcons name="bike" color={white} size={32} />
           </View>
-        )
+        );
       }
     },
     swim: {
@@ -67,14 +66,10 @@ export function getMetricMetaInfo (metric) {
       type: 'steppers',
       getIcon() {
         return (
-          <View style={[styles.iconContainer, {backgroundColor: blue}]}>
-            <MaterialCommunityIcons
-              name='swim'
-              color={white}
-              size={35}
-            />
+          <View style={[styles.iconContainer, { backgroundColor: blue }]}>
+            <MaterialCommunityIcons name="swim" color={white} size={35} />
           </View>
-        )
+        );
       }
     },
     sleep: {
@@ -85,14 +80,10 @@ export function getMetricMetaInfo (metric) {
       type: 'slider',
       getIcon() {
         return (
-          <View style={[styles.iconContainer, {backgroundColor: lightPurp}]}>
-            <FontAwesome
-              name='bed'
-              color={white}
-              size={30}
-            />
+          <View style={[styles.iconContainer, { backgroundColor: lightPurp }]}>
+            <FontAwesome name="bed" color={white} size={30} />
           </View>
-        )
+        );
       }
     },
     eat: {
@@ -103,62 +94,108 @@ export function getMetricMetaInfo (metric) {
       type: 'slider',
       getIcon() {
         return (
-          <View style={[styles.iconContainer, {backgroundColor: pink}]}>
-            <MaterialCommunityIcons
-              name='food'
-              color={white}
-              size={35}
-            />
+          <View style={[styles.iconContainer, { backgroundColor: pink }]}>
+            <MaterialCommunityIcons name="food" color={white} size={35} />
           </View>
-        )
+        );
       }
-    },
-  }
+    }
+  };
 
-  return typeof metric === 'undefined'
-    ? info
-    : info[metric]
+  return typeof metric === 'undefined' ? info : info[metric];
 }
 
-
-export function isBetween (num, x, y) {
+export function isBetween(num, x, y) {
   if (num >= x && num <= y) {
-    return true
+    return true;
   }
 
-  return false
+  return false;
 }
 
-export function calculateDirection (heading) {
-  let direction = ''
+export function calculateDirection(heading) {
+  let direction = '';
 
   if (isBetween(heading, 0, 22.5)) {
-    direction = 'North'
+    direction = 'North';
   } else if (isBetween(heading, 22.5, 67.5)) {
-    direction = 'North East'
+    direction = 'North East';
   } else if (isBetween(heading, 67.5, 112.5)) {
-    direction = 'East'
+    direction = 'East';
   } else if (isBetween(heading, 112.5, 157.5)) {
-    direction = 'South East'
+    direction = 'South East';
   } else if (isBetween(heading, 157.5, 202.5)) {
-    direction = 'South'
+    direction = 'South';
   } else if (isBetween(heading, 202.5, 247.5)) {
-    direction = 'South West'
+    direction = 'South West';
   } else if (isBetween(heading, 247.5, 292.5)) {
-    direction = 'West'
+    direction = 'West';
   } else if (isBetween(heading, 292.5, 337.5)) {
-    direction = 'North West'
+    direction = 'North West';
   } else if (isBetween(heading, 337.5, 360)) {
-    direction = 'North'
+    direction = 'North';
   } else {
-    direction = 'Calculating'
+    direction = 'Calculating';
   }
 
-  return direction
+  return direction;
 }
 
-export function timeToString (time = Date.now()) {
-  const date = new Date(time)
-  const todayUTC = new Date(Date.UTC(date.getFullYear(), date.getMonth(), date.getDate()))
-  return todayUTC.toISOString().split('T')[0]
+export function timeToString(time = Date.now()) {
+  const date = new Date(time);
+  const todayUTC = new Date(
+    Date.UTC(date.getFullYear(), date.getMonth(), date.getDate())
+  );
+  return todayUTC.toISOString().split('T')[0];
+}
+
+export function clearLocalNotification() {
+  return AsyncStorage.removeItem(NOTIFICATION_KEY)
+    .then(Notifications.cancelAllScheduledNotificationsAsync)
+}
+
+function createNotification () {
+  return {
+    title: 'Log your stats!',
+    body: "👋 don't forget to log your stats for today!",
+    ios: {
+      sound: true,
+    },
+    android: {
+      sound: true,
+      priority: 'high',
+      sticky: false,
+      vibrate: true,
+    }
+  }
+}
+
+export function setLocalNotification () {
+  AsyncStorage.getItem(NOTIFICATION_KEY)
+    .then(JSON.parse)
+    .then((data)=>{
+      if (data === null) {
+        Permissions.askAsync(Permissions.NOTIFICATIONS)
+          .then(({ status }) => {
+            if (status === 'granted') {
+              Notifications.cancelAllScheduledNotificationsAsync()
+
+              let tomorrow = new Date()
+              tomorrow.setDate(tomorrow.getDate() + 1)
+              tomorrow.setHours(20)
+              tomorrow.setMinutes(0)
+
+              Notifications.scheduleLocalNotificationAsync(
+                createNotification(),
+                {
+                  time: tomorrow,
+                  repeat: 'day'
+                }
+              )
+
+              AsyncStorage.setItem(NOTIFICATION_KEY, JSON.stringify(true))
+            }
+          }) 
+      }
+    })
 }
